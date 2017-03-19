48c61d665ba6612986c8c6f9152a9ec738efd257
delola\delola lokal <aleksandra.man@gmail.com>
2017-03-19 21:44:23 +0100

    added window.onload function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-	getJoke();
+
+	var url = "http://api.icndb.com/jokes/random";
+	var paragraph = document.getElementById("joke");
+	var button = document.getElementById("get-joke");
+
+	button.addEventListener("click", getJoke);
+
+	function getJoke() {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url);
+		xhr.addEventListener("load", function () {
+			var response = JSON.parse(xhr.response);
+			paragraph.innerHTML = response.value.joke;
+		});
+		xhr.send();
+	}
+
+	window.onload = getJoke();
 });
-
-var url = "http://api.icndb.com/jokes/random";
-var paragraph = document.getElementById("joke");
-var button = document.getElementById("get-joke");
-
-button.addEventListener("click", function(){
-	getJoke();
-});
-
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.addEventListener("load", function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
-	});
-	xhr.send();
-}
